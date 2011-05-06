056ddf98b909811fba57085ddc9ead06247d6c01
Rafael George <george.rafael@gmail.com>
2011-05-06 18:35:16 +0000

    added mock get request for new assignee task fixture

--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -11,6 +11,7 @@ describe "Ticketmaster::Provider::Kanbanpad::Ticket" do
       mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks.json?backlog=yes&finished=yes', wheaders, fixture_for('tasks'), 200
       mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007.json', wheaders, fixture_for('tasks/4cd428c496f0734eef000007'), 200
       mock.get '/api/v1/projects/be74b643b64e3dc79aa0/steps/4dc312f49bd0ff6c37000040.json', wheaders, fixture_for('steps/4dc312f49bd0ff6c37000040'), 200
+      mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks/4dc31c4c9bd0ff6c3700004e.json', wheaders, fixture_for('tasks/4dc31c4c9bd0ff6c3700004e'), 200
     end
     @project_id = 'be74b643b64e3dc79aa0'
     @ticket_id = '4cd428c496f0734eef000007'
@@ -61,6 +62,7 @@ describe "Ticketmaster::Provider::Kanbanpad::Ticket" do
 
   it "should return nobody as assignee for an empty assignee from the api" do 
     @ticket = @project.ticket(@ticket_id_without_assignee)
+    @ticket.assignee.should == 'nobody'
   end
 
 end
