233c9a7b49e11fe6d35e09e58ee1879bb801dc74
Rafael George <george.rafael@gmail.com>
2011-12-16 22:32:33 -0400

    added top level definition for tickets and comments specs

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Ticketmaster::Provider::Kanbanpad::Comment" do
+describe TicketMaster::Provider::Kanbanpad::Comment do
   before(:all) do
     headers = {'Authorization' => 'Basic YWJjQGcuY29tOmllODIzZDYzanM='}
     wheaders = headers.merge('Accept' => 'application/json')
--- spec/tickets_spec.rb
+++ spec/tickets_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Ticketmaster::Provider::Kanbanpad::Ticket" do
+describe TicketMaster::Provider::Kanbanpad::Ticket do
   before(:all) do
     headers = {'Authorization' => 'Basic YWJjQGcuY29tOmllODIzZDYzanM='}
     wheaders = headers.merge('Accept' => 'application/json')
