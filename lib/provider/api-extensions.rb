a93ea4426d6f6b9cf5141efe9918602bba87ad54
Rafael George <george.rafael@gmail.com>
2012-04-25 19:45:19 +0000

    switch task resource for updating in kanbanpad api

--- lib/provider/api-extensions.rb
+++ lib/provider/api-extensions.rb
@@ -6,18 +6,15 @@ module APIExtentions
     :updated_at => updated_at, 
     :wip => wip, 
     :step_id => step_id, 
-    :project_slug => project_slug, 
+    :project_slug => project_slug,
     :finished => finished, 
     :backlog => backlog, 
     :assignee => assigned_to}
   end
 
   def update_with(ticket)
-    self.assigne_to = ticket.assignee
+    self.assigned_to = ticket.assignee == 'Nobody' ? '' : ticket.assignee
     self.title = ticket.title
-    self.project_id = ticket.project_slug
-    self.step_id = ticket.step_id
-    self.prefix_options = {:step_id => ticket.step_id, :project_id => ticket.project_id}
     self.note = ticket.description
     self
   end
