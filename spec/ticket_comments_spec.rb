543731f6819359feb6d0265fca3f63dc0278bc19
Rafael George <george.rafael@gmail.com>
2012-06-13 14:53:31 +0000

    Refactor ticket_comment creation specs * Use more let and describe blocks to better separation of concerns

--- spec/ticket_comments_spec.rb
+++ spec/ticket_comments_spec.rb
@@ -1,55 +1,68 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TaskMapper::Provider::Kanbanpad::Comment do
-  before(:all) do
-    headers = {'Authorization' => 'Basic YWJjQGcuY29tOmllODIzZDYzanM='}
-    wheaders = headers.merge('Accept' => 'application/json')
-    pheaders = headers.merge("Content-Type" => "application/json")
-    
-    ActiveResource::HttpMock.respond_to do |mock|
-      mock.get '/api/v1/projects/be74b643b64e3dc79aa0.json', wheaders, fixture_for('projects/be74b643b64e3dc79aa0'), 200
-      mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks.json', wheaders, fixture_for('tasks'), 200
-      mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks.json?backlog=yes&finished=yes', wheaders, fixture_for('tasks'), 200
-      mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007.json', wheaders, fixture_for('tasks/4cd428c496f0734eef000007'), 200
-      mock.get '/api/v1/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007/comments.json', wheaders, fixture_for('comments'), 200
-      mock.post '/api/v1/projects/be74b643b64e3dc79aa0/steps/4dc312f49bd0ff6c37000040/tasks/4cd428c496f0734eef000007/comments.json', pheaders, fixture_for('comments/4ef2719bf17365000110df9e'), 200
+  let(:headers) { {'Authorization' => 'Basic YWJjQGcuY29tOmllODIzZDYzanM='} }
+  let(:wheaders) { headers.merge('Accept' => 'application/json') }
+  let(:pheaders) { headers.merge("Content-Type" => "application/json") }
+  let(:project_id) { 'be74b643b64e3dc79aa0' }
+  let(:ticket_id) { '4cd428c496f0734eef000007' }
+  let(:comment_id) { '4d684e6f973c7d5648000009' }
+  let(:tm) { TaskMapper.new(:kanbanpad, :username => 'abc@g.com', :password => 'ie823d63js') }
+  let(:comment_class) { TaskMapper::Provider::Kanbanpad::Comment }
+
+  describe "Retrieving ticket comments" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects/be74b643b64e3dc79aa0.json', wheaders, fixture_for('projects/be74b643b64e3dc79aa0'), 200
+        mock.get '/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007.json', wheaders, fixture_for('tasks/4cd428c496f0734eef000007'), 200
+        mock.get '/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007/comments.json', wheaders, fixture_for('comments'), 200
+      end
     end
-    @project_id = 'be74b643b64e3dc79aa0'
-    @ticket_id = '4cd428c496f0734eef000007'
-    @comment_id = '4d684e6f973c7d5648000009'
+
+    let(:project) { tm.project project_id }
+    let(:ticket) { project.ticket ticket_id }
+
+    context "when calling #comments to a ticket instance" do 
+      subject { ticket.comments } 
+      it { should be_an_instance_of Array }
+      it { subject.first.should be_an_instance_of comment_class }
+    end
+
+    context "when calling #comments with an array of comments id's" do 
+      subject { ticket.comments [comment_id] } 
+      it { should be_an_instance_of Array } 
+      it { subject.first.should be_an_instance_of comment_class } 
+      it { subject.first.id.should be_eql comment_id }
+    end
+
+    context "when calling #comments with a hash attributes" do 
+      subject { ticket.comments :id => comment_id } 
+      it { should be_an_instance_of Array }
+      it { subject.first.should be_an_instance_of comment_class } 
+      it { subject.first.id.should be_eql comment_id } 
+    end
+
   end
 
-  before(:each) do
-    @taskmapper = TaskMapper.new(:kanbanpad, :username => 'abc@g.com', :password => 'ie823d63js')
-    @project = @taskmapper.project(@project_id)
-    @ticket = @project.ticket(@ticket_id)
-    @klass = TaskMapper::Provider::Kanbanpad::Comment
+  describe "Comment creation to a ticket" do 
+    before(:each) do 
+      ActiveResource::HttpMock.respond_to do |mock|
+        mock.get '/projects/be74b643b64e3dc79aa0.json', wheaders, fixture_for('projects/be74b643b64e3dc79aa0'), 200
+        mock.get '/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007.json', wheaders, fixture_for('tasks/4cd428c496f0734eef000007'), 200
+        mock.get '/projects/be74b643b64e3dc79aa0/tasks/4cd428c496f0734eef000007/comments.json', wheaders, fixture_for('comments'), 200
+        mock.post '/projects/be74b643b64e3dc79aa0/steps/4dc312f49bd0ff6c37000040/tasks/4cd428c496f0734eef000007/comments.json', pheaders, '', 200
+      end
+    end
+
+    let(:project) { tm.project project_id }
+    let(:ticket) { project.ticket ticket_id }
+
+    context "when calling #comment! to a ticket instance" do 
+      subject { ticket.comment! :body => 'New Ticket' }
+      it { should be_an_instance_of comment_class }
+      it { subject.ticket_id.should_not be_nil }
+      it { subject.ticket_id.should_not be_eql 0 }
+    end
   end
 
-  it "should be able to load all comments" do 
-    @comments = @ticket.comments
-    @comments.should be_an_instance_of(Array)
-    @comments.first.should be_an_instance_of(@klass)
-  end
-
-  it "should be able to load all comments based on array of id's" do 
-    @comments = @ticket.comments([@comment_id])
-    @comments.should be_an_instance_of(Array)
-    @comments.first.should be_an_instance_of(@klass)
-  end
-
-  it "should be able to load all comments based on attributes" do 
-    @comments = @ticket.comments(:id => @comment_id)
-    @comments.should be_an_instance_of(Array)
-    @comments.first.should be_an_instance_of(@klass)
-  end
-
-  it "should be able to create a comment for a given task" do
-    comment = @ticket.comment!(:body => "New Ticket")
-    comment.should be_an_instance_of(@klass)
-    
-    comment.ticket_id.should_not be_nil
-    comment.ticket_id.should_not == 0
-  end
-  
 end
