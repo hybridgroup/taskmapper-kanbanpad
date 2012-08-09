5ea6903ec211971129345532cf33371997bbd436
Omar Rodriguez <omarjavier15@gmail.com>
2012-08-09 10:39:31 +0000

    Refactor: Extract http stuffs to Middleware::HTTP The idea is to make tm-kanbanpad depend only on Middleware and eventualy get Faraday out of tm-kanbanpad

--- lib/taskmapper/providers/kanbanpad/projects.rb
+++ lib/taskmapper/providers/kanbanpad/projects.rb
@@ -1,15 +1,39 @@
 module TaskMapper
   module Providers
-    module Kanbanpad
-      module Projects
-        def search(criteria)
-          con = Faraday.new(:url => 'https://www.kanbanpad.com') do |c|
+    module Middleware
+      module HTTP
+        def get(url)
+          connection.get("#{base_path}#{url}").body
+        end
+        
+        def connection
+          con = Faraday.new(:url => domain) do |c|
             c.adapter Faraday.default_adapter
             c.basic_auth(credentials[:username], credentials[:password])
             c.response :json, :content_type => /\bjson$/
           end
-          
-          con.get('/api/v1/projects.json').body.map do |kpp|
+        end
+      end
+    end
+  end
+end
+
+module TaskMapper
+  module Providers
+    module Kanbanpad
+      module Projects
+        include Middleware::HTTP
+        
+        def domain
+          'https://www.kanbanpad.com'
+        end
+        
+        def base_path
+          '/api/v1'
+        end
+        
+        def search(criteria)
+          get('/projects.json').map do |kpp|
             {
               :name       => kpp["name"],
               :created_at => Time.new(kpp["created_at"]),
