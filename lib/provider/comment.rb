e7e38aa62aa69916f44ab53e55df087c5398c6b7
Andrew Stewart <andrew@stwrt.ca>
2013-10-08 11:36:49 -0700

    Refactor and clean up Comment class

--- lib/provider/comment.rb
+++ lib/provider/comment.rb
@@ -1,43 +1,21 @@
 module TaskMapper::Provider
   module Kanbanpad
-    # The comment class for taskmapper-kanbanpad
-    #
-    # Do any mapping between taskmapper and your system's comment model here
-    # versions of the ticket.
-    #
-    # Not supported by Kanbanpad API
     class Comment < TaskMapper::Provider::Base::Comment
-      # declare needed overloaded methods here
       API = KanbanpadAPI::TaskComment
 
-      def initialize(*object)
-        if object.first
-          object = object.first
-          unless object.is_a? Hash
-            hash = {:id => object.id,
-                    :author => object.author,
-                    :body => object.body,
-                    :created_at => object.created_at,
-                    :updated_at => object.updated_at}
-          else
-            hash = object
-          end
-          super hash
-        end
+      # Public: Creates a new Comment based on a Hash of attributes
+      #
+      # comment - hash of Comment attributes
+      #
+      # Returns a new Comment
+      def initialize(*comment)
+        comment = comment.first if comment.is_a?(Array)
+        super comment
       end
 
-      def self.find_by_id(project_id, ticket_id, id)
-        self.search(project_id, ticket_id).select { |ticket| ticket.id == id }.first
-      end
-
-      def self.find_by_attributes(project_id, ticket_id, attributes = {})
-        search_by_attribute(self.search(project_id, ticket_id), attributes)
-      end
-
-      def self.search(project_id, ticket_id, options = {}, limit = 1000)
-        comments = API.find(:all, :params => {:project_id => project_id, :task_id => ticket_id}).collect { |comment| self.new comment }
-      end
-
+      # Public: Gives an updated_at Time for the Comment.
+      #
+      # Returns a Time object or a string if it couldn't be parsed
       def updated_at
         @updated_at ||= begin
           Time.parse(self[:updated_at])
@@ -46,22 +24,74 @@ module TaskMapper::Provider
         end
       end
 
+      # Public: Gives an created_at Time for the Comment.
+      #
+      # Returns a Time object or a string if it couldn't be parsed
       def created_at
-        @updated_at ||= begin
+        @created_at ||= begin
           Time.parse(self[:created_at])
         rescue
           self[:created_at]
         end
       end
 
-      def self.create(project_id, ticket_id, step_id, *options)
-        options.first.merge!(
-                       :project_id => project_id,
-                       :task_id => ticket_id,
-                       :step_id => step_id)
-        task_comment = KanbanpadAPI::TaskCommentCreator.new(options.first)
-        task_comment.save
-        self.new task_comment
+      class << self
+        # Public: Finds a specific Comment by it's project/ticket/comment IDs
+        #
+        # project_id - the ID of the project the comment belongs to
+        # ticket_id - the ID of the ticket the comment belongs to
+        # comment_id - the ID of the comment to find
+        #
+        # Returns the requested Comment instance
+        def find_by_id(project_id, ticket_id, id)
+          find_by_attributes(project_id, ticket_id, :id => id).first
+        end
+
+        # Public: Finds all Comments that match an array of attributes
+        #
+        # project_id - the ID of the project the ticket belongs to
+        # ticket_id - the ID of the ticket to find comments for
+        # attributes - a hash of attributes to search with
+        #
+        # Returns an Array of matching comments
+        def find_by_attributes(project_id, ticket_id, attributes = {})
+          search_by_attribute(self.find_all(project_id, ticket_id), attributes)
+        end
+
+        # Public: Finds all Comments that belong to a project/ticket
+        #
+        # project_id - the ID of the project the ticket belongs to
+        # ticket_id - the ID of the ticket to find comments for
+        #
+        # Returns an Array of all comments
+        def find_all(project_id, ticket_id)
+          comments = API.find(
+            :all,
+            :params => { :project_id => project_id, :task_id => ticket_id }
+          )
+
+          comments.collect { |comment| self.new comment.attributes }
+        end
+
+        # Public: Creates a new Comment based on passed attributes
+        #
+        # project_id - the ID of the project the ticket belongs to
+        # ticket_id - the ID of the ticket to create a comment for
+        # step_id - the step to create a comment for
+        # attrs - hash of Comment attributes
+        #
+        # Returns a new Comment instance
+        def create(project_id, ticket_id, step_id, attrs)
+          attrs.merge!(
+            :project_id => project_id,
+            :task_id => ticket_id,
+            :step_id => step_id
+          )
+
+          task_comment = KanbanpadAPI::TaskCommentCreator.new attrs
+          task_comment.save
+          self.new task_comment
+        end
       end
     end
   end
